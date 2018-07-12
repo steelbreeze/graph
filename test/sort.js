f730a9f607fe5df7c2126c866012d937f1bfaaae
mesmo <mesmo@steelbreeze.net>
2018-07-12 17:43:41 +0100

    Added test for circular dependency check in tolopogical sort

--- test/sort.js
+++ test/sort.js
@@ -2,25 +2,41 @@ const assert = require("assert");
 const graph = require('../lib/node/index');
 
 const data = [
-	{ name: 'A', requires: ['E', 'B'], toString: () => 'A' },
-	{ name: 'B', requires: ['D', 'C'], toString: () => 'B' },
-	{ name: 'C', requires: [], toString: () => 'C' },
-	{ name: 'D', requires: [], toString: () => 'D' },
-	{ name: 'E', requires: [], toString: () => 'E' }
+	{ name: 'A', requires: ['E', 'B'] },
+	{ name: 'B', requires: ['D', 'C'] },
+	{ name: 'C', requires: [] },
+	{ name: 'D', requires: [] },
+	{ name: 'E', requires: [] }
 ];
 
-function dependencies(i) {
+const circle = [
+	{ name: 'A', requires: ['B'] },
+	{ name: 'B', requires: ['C'] },
+	{ name: 'C', requires: ['A'] }
+];
+
+function dependencies1(i) {
 	return data.filter(j => i.requires.indexOf(j.name) !== -1);
 }
 
-describe("test/sort.js", function () {
-	it("topological", function () {
-		const sorted = graph.Sort.topological(data, dependencies, true);
+function dependencies2(i) {
+	return circle.filter(j => i.requires.indexOf(j.name) !== -1);
+}
 
-		assert.equal('C', sorted[0].name);
-		assert.equal('D', sorted[1].name);
-		assert.equal('B', sorted[2].name);
-		assert.equal('E', sorted[3].name);
-		assert.equal('A', sorted[4].name);
+describe("test/sort.js", function () {
+	it("topological/sort", function () {
+		const sorted1 = graph.Sort.topological(data, dependencies1, true);
+
+		assert.equal('C', sorted1[0].name);
+		assert.equal('D', sorted1[1].name);
+		assert.equal('B', sorted1[2].name);
+		assert.equal('E', sorted1[3].name);
+		assert.equal('A', sorted1[4].name);
+	});
+
+	it("topological/circular", function () {
+		assert.throws(() => {
+			const sorted2 = graph.Sort.topological(circle, dependencies2, true);
+		}, Error, "Topologicalt sort: circular dependency detected");
 	});
 });
